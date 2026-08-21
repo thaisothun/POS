c2184afcdb503a00476d3f360f6536be5b0209e7
thaisothun <thaisothun@gmail.com>
2026-08-21 23:17:11 +0700

    update

--- core/static/js/category_filter.js
+++ core/static/js/category_filter.js
@@ -105,14 +105,14 @@ form_update.addEventListener('submit', async function(e){
                     title: "Update Item",
                     text: data.message_error,
                     icon: "error"
-                    }); window.location.reload();
+                    }); window.addEventListener('click', ()=>{ window.location.reload();})
             }
             if (data.message_sucess){
                 Swal.fire({
                     title: "Update Item",
                     text: data.message_sucess,
                     icon: "success"
-                    }); window.location.reload();
+                    }); window.addEventListener('click', ()=>{ window.location.reload();})
             }
         } catch (error) {
         console.error('Fetch error:', error);}
